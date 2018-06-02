b46e5a6b07a1d25d1056765b22b99781b33bc285
Matt Crawford <mlctrez@gmail.com>
2018-06-02 14:27:46 -0500

    initial version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module wakeup
+
+require (
+	github.com/linde12/gowol v0.0.0-20170712070842-71887642b739
+)
---
+++ wakeup.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/linde12/gowol"
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) == 0 {
+		fmt.Println("provide one or more short host names as arguments")
+		os.Exit(1)
+	}
+
+	resp, err := http.Get("http://pe0:10001/hosts")
+	if err != nil {
+		log.Fatal(err)
+	}
+	api := &HostsApi{}
+	err = json.NewDecoder(resp.Body).Decode(api)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, h := range api.Hosts {
+		for _, a := range args {
+			if strings.ToLower(h.Name) == strings.ToLower(a) {
+				packet, err := gowol.NewMagicPacket(h.MacAddress)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println("wakeup", h.Name, "at", h.MacAddress)
+				err = packet.Send("255.255.255.255")
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+
+	}
+
+}
+
+type HostsApi struct {
+	Hosts []*Host
+}
+type Host struct {
+	Name       string `json:"_id"`
+	IpAddress  string `json:"ip_address"`
+	MacAddress string `json:"mac_address"`
+}
